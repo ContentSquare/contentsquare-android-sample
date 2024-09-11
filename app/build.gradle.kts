7aafd6e818e4cffa8a8029214c2f2870f46d850b
Pat <64789060+weeraphat-kaewnang@users.noreply.github.com>
2024-09-11 10:57:54 +0200

    Update Contentsquare unified SDK 0.1.1 (#2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 }
 
 dependencies {
-    implementation("com.contentsquare.android:sdk:0.1.0")
+    implementation("com.contentsquare.android:sdk:0.1.1")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
