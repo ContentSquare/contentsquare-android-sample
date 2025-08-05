a61b7f3d298d73f5be7ff3551d33fbce3a94d64c
Michael Celey <michael.celey@heapanalytics.com>
2025-08-05 15:15:38 -0400

    Enabling PA in the sample app to show customers how to initialize with autocapture. The supplied environment ID is for a new environment that was created specifically to house sample app data and keep it separate from other environments.
    refs MOBILE-17358

--- app/src/main/java/com/contentsquare/android/sample/SampleApplication.kt
+++ app/src/main/java/com/contentsquare/android/sample/SampleApplication.kt
@@ -5,14 +5,22 @@ import android.app.Application
 import android.os.Bundle
 import com.contentsquare.CSQ
 import com.contentsquare.android.sample.crash.CrashHelper
+import com.contentsquare.api.model.ProductAnalyticsOptions
 
 class SampleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        // If you want to also activate Product Analytics, you have to uncomment this line and update the ENV_ID.
-        // CSQ.configureProductAnalytics(this, "ENV_ID", /* options */)
+        CSQ.configureProductAnalytics(
+            context = this,
+            envId = "3521501044",
+            options = ProductAnalyticsOptions(
+                enableViewAutocapture = true,
+                // Pageview capture is turned off in favor of manual screen tracking in this sample.
+                disablePageviewAutocapture = true
+            )
+        )
         CSQ.start(this)
         CSQ.optIn(this)
         CrashHelper.init(this)
