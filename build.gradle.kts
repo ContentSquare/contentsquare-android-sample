c3d0c026486ecf644148dd5facdfce5309ed0417
Arthur Cugniere <arthur.cugniere@contentsquare.com>
2026-06-05 15:17:51 +0200

    update error analysis plugins to crash 1.9.0 and network 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
-    id("com.contentsquare.error.analysis.network") version "1.7.0" apply false
-    id("com.contentsquare.error.analysis.crash") version "1.8.0" apply false
+    id("com.contentsquare.error.analysis.network") version "1.8.0" apply false
+    id("com.contentsquare.error.analysis.crash") version "1.9.0" apply false
 }
